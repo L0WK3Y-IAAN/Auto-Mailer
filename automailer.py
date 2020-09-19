f541c5e7b14551368b92f36351d3a2fe7efa8e2b
L0WK3Y | I.A.A.N <32310877+L0WK3Y-IAAN@users.noreply.github.com>
2020-09-19 16:14:18 -0400

    Update automailer.py

--- automailer.py
+++ automailer.py
@@ -1,17 +1,27 @@
 """
-[+] Are you annoyed with the tedious task of sending repetative daily emails? Use Automailer!
-
-
-AUTOMAILER by... 
+AUTOMAILER by...
  _       __   __      __ _  __   ____ __   __
 | |     /  \  \ \    / /| |/ /  |__ / \ \ / /
 | |__  | () |  \ \/\/ / |   <    |_ \  \   / 
 |____|  \__/    \_/\_/  |_|\_\  |___/   |_|  
 
-
+[+] Are you annoyed with the tedious task of sending repetative daily emails? Use Automailer!
+ 
 This script can be used to automatically send scheduled emails to any email of your choosing. 
 Since I was not planning on releasing this publicly, a few of the variables are hard coded. 
 I will change the variables to take user-input in a future release, or you can change them until then with this source code.
+Once you've set your variables to your liking, just throw it onto a server, run it and you're good to go. :)
+
+
+Required PIP packages to be installed:
+
+Schedule: https://pypi.org/project/schedule/
+Twilio: https://pypi.org/project/twilio/
+
+or use
+
+"pip install -r requirements.txt" (Paste in CLI)
+
 
 Enjoy :)
 """
